af731208975b329892dc8051caf8569188b36b84
Venkateswaralu Prasadrao <42943348+vprasadr@users.noreply.github.com>
2024-11-11 18:42:34 +0100

    Update perseus_pii_function.py

--- perseus_pii_function.py
+++ perseus_pii_function.py
@@ -1,13 +1,20 @@
+import bigframes.pandas as bf
+import pandas as pd
 from bigframes.dataframe import DataFrame
 from typing import Any, Callable, Dict, List, Union
+from presidio_structured import StructuredEngine, PandasAnalysisBuilder
+from presidio_anonymizer.entities import OperatorConfig
 bf.close_session()
 bf.options.bigquery.location = "us" #this variable is set based on the dataset you chose to query
-bf.options.bigquery.project = "perseus-curation-stg-1274" #this variable is set based on the dataset you chose to query
-from presidio_analyzer import AnalyzerEngine
+bf.options.bigquery.project = "dh-data-platform-analytics" #this variable is set based on the dataset you chose to query
+from presidio_analyzer import AnalyzerEngine, RecognizerRegistry
 from presidio_anonymizer import AnonymizerEngine
 
+registry = RecognizerRegistry()
+registry.load_predefined_recognizers(languages=['en'])
+
 # Set up analyzer with our updated recognizer registry
-analyzer = AnalyzerEngine()
+analyzer = AnalyzerEngine(registry=registry)
 anonymizer = AnonymizerEngine()
 
 #@bf.remote_function([str, str], str, packages=["presidio_anonymizer", "presidio_analyzer"], dataset='test', name='hash_pii_venkat', reuse=True, bigquery_connection='perseus-curation-stg-1274.us.perseus-curation-stg-1274_cloud_resource_connection', cloud_function_memory_mib='3000', max_batching_rows=1000)
