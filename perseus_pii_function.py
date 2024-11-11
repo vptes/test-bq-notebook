2d8b2d54de3ca9d96dffe181fa2509929fa548c9
Venkateswaralu Prasadrao <42943348+vprasadr@users.noreply.github.com>
2024-11-11 18:38:41 +0100

    Update perseus_pii_function.py

--- perseus_pii_function.py
+++ perseus_pii_function.py
@@ -10,21 +10,11 @@ from presidio_anonymizer import AnonymizerEngine
 analyzer = AnalyzerEngine()
 anonymizer = AnonymizerEngine()
 
-#@bf.remote_function([str, str], str, packages=["presidio_anonymizer", "presidio_analyzer"], dataset='test', name='hash_pii_venkat', reuse=True, bigquery_connection='perseus-curation-stg-1274.us.perseus-curation-stg-1274_cloud_resource_connection', cloud_function_memory_mib='3000', max_batching_rows=1000)
-def hash_pii_using_presidio('venk.113@gmail.com', entities='EMAIL_ADDRESS'):
-
-    if entities is None:
-        entities = ['IBAN_CODE', "EMAIL_ADDRESS", "PHONE_NUMBER", "CREDIT_CARD", 'IP_ADDRESS', 'PERSON']
-    else:
-        entities = entities.split(',')
-
-    # If the text contains an email address, replace only %40 with @
-    if 'EMAIL_ADDRESS' in entities:
-        text = text.replace('%40', '@')  # Replace only %40 with @
+#@bf.remote_function([str], str, packages=["presidio_anonymizer", "presidio_analyzer"], dataset='test', name='hash_pii_venkat', reuse=True, bigquery_connection='perseus-curation-stg-1274.us.perseus-curation-stg-1274_cloud_resource_connection', cloud_function_memory_mib='3000', max_batching_rows=1000)
+def hash_pii_using_presidio(text):
 
     # Call analyzer to get results
     analyzer_results = analyzer.analyze(text=text,
-                                        entities=entities,
                                         language='en')
 
     # Analyzer results are passed to the AnonymizerEngine for anonymization
