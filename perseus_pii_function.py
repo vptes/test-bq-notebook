a1c9e3b36b5eaf43d8f754a00ccb0957a7d7997c
Venkateswaralu Prasadrao <42943348+vprasadr@users.noreply.github.com>
2024-11-11 18:32:49 +0100

    Update perseus_pii_function.py

--- perseus_pii_function.py
+++ perseus_pii_function.py
@@ -1,21 +1,33 @@
+from bigframes.dataframe import DataFrame
+from typing import Any, Callable, Dict, List, Union
+bf.close_session()
+bf.options.bigquery.location = "us" #this variable is set based on the dataset you chose to query
+bf.options.bigquery.project = "perseus-curation-stg-1274" #this variable is set based on the dataset you chose to query
 from presidio_analyzer import AnalyzerEngine
 from presidio_anonymizer import AnonymizerEngine
 
-# Initialize the analyzer and anonymizer
+# Set up analyzer with our updated recognizer registry
 analyzer = AnalyzerEngine()
 anonymizer = AnonymizerEngine()
 
-# Sample text to analyze
-text = "My phone number is 123-456-7890."
+@bf.remote_function([str, str], str, packages=["presidio_anonymizer", "presidio_analyzer"], dataset='test', name='hash_pii_venkat', reuse=True, bigquery_connection='perseus-curation-stg-1274.us.perseus-curation-stg-1274_cloud_resource_connection', cloud_function_memory_mib='3000', max_batching_rows=1000)
+def hash_pii_using_presidio(text, entities=None):
 
-# Analyze the text
-results = analyzer.analyze(text=text, language='en')
+    if entities is None:
+        entities = ['IBAN_CODE', "EMAIL_ADDRESS", "PHONE_NUMBER", "CREDIT_CARD", 'IP_ADDRESS', 'PERSON']
+    else:
+        entities = entities.split(',')
 
-# Print the results of the analysis
-for result in results:
-    print(f"Entity: {result.entity_type}, Score: {result.score}, Position: {result.start}-{result.end}")
+    # If the text contains an email address, replace only %40 with @
+    if 'EMAIL_ADDRESS' in entities:
+        text = text.replace('%40', '@')  # Replace only %40 with @
 
-# Anonymize the text (optional)
-anonymized_text = anonymizer.anonymize(text=text, analyzer_results=results)
+    # Call analyzer to get results
+    analyzer_results = analyzer.analyze(text=text,
+                                        entities=entities,
+                                        language='en')
 
-print(f"Anonymized Text: {anonymized_text.text}")
+    # Analyzer results are passed to the AnonymizerEngine for anonymization
+    anonymized_text = anonymizer.anonymize(text=text, analyzer_results=analyzer_results)
+
+    return anonymized_text.text
